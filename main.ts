d93f5f62043c8550ef9d77667cb43c0c4ed7c2c6
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2023-01-18 15:43:01 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,51 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showNumber(Bodenfeuchte)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+function Pumpe_aus () {
+    Pumpenstatus = 0
+    pins.analogWritePin(AnalogPin.P2, Pumpenstatus)
+}
+function Pumpe_an () {
+    Pumpenstatus = 1023
+    pins.analogWritePin(AnalogPin.P2, Pumpenstatus)
+    Sekunden_seit_gießen = 0
+}
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    Sekunden_seit_gießen = WartezeitNachGiessenInSekunden
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showNumber(Sekunden_seit_gießen)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+function messe_Bodenfeuchte () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    Bodenfeuchte = pins.analogReadPin(AnalogPin.P1)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+}
+let Pumpsekunden = 0
+let Pumpenstatus = 0
+let Bodenfeuchte = 0
+let Sekunden_seit_gießen = 0
+let WartezeitNachGiessenInSekunden = 0
+WartezeitNachGiessenInSekunden = 1 * 60
+let GrenzwertBodenfeuchte = 500
+let DauerPumpenInSekunden = 10
+Sekunden_seit_gießen = 0
+loops.everyInterval(1000, function () {
+    if (Pumpsekunden == 0) {
+        Pumpe_aus()
+    } else {
+        Pumpe_an()
+        Pumpsekunden += -1
+    }
+    if (Sekunden_seit_gießen >= WartezeitNachGiessenInSekunden) {
+        messe_Bodenfeuchte()
+        if (Bodenfeuchte < GrenzwertBodenfeuchte) {
+            Pumpsekunden = DauerPumpenInSekunden
+        }
+    }
+    Sekunden_seit_gießen += 1
+})
